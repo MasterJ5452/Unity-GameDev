be88b16e27790e16b4e69127bf9762195ad2f2c8
Jamie Saker <161651635+MasterJ5452@users.noreply.github.com>
2026-01-30 10:16:42 -0600

    Made some builds today and found bugs
    I made my first 2 builds of the game and discovered some bugs. The main one was that the Player could fire multiple times on the same enemy after it was destroyed causing the Player to be able to rack up points but also still did damage if the Player flew through the area. I fixed it by destroying the 2d collider right after it was hit by either the Player or the laser. While still leaving the object instantiated until all animations a audio got done playing.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,32 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4f;
+    [SerializeField]
+    private float _nextFireTime;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private AudioSource _laserAudio;
 
     private Player _player;
     private Animator _anim;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-   private AudioSource _audioSource;
+    
     //Audio
+   private AudioSource _audioSource;
+
+
     
     void Start()
     {
         
         _player = GameObject.Find("Player").GetComponent<Player>();
         _audioSource = GetComponent<AudioSource>();
+        _laserAudio = GameObject.Find("Laser_Shot").GetComponent<AudioSource>();
         //null check player
         if(_player == null)
         {
@@ -39,12 +49,27 @@ public class Enemy : MonoBehaviour
         transform.Translate(Vector2.down * _speed * Time.deltaTime);
         //if bottom of screen 
         //respawn at top with a new random x position
+        
         if(transform.position.y <= -5.4f)
         {
             float randomX = Random.Range(-9.5f, 9.5f);
             float randomY = Random.Range(7.4f, 10f);
             transform.position = new Vector2(randomX, randomY);
         }
+
+        if(Time.time >= _nextFireTime)
+        {
+            FireLaser();
+            _nextFireTime = Time.time + Random.Range(3,8);
+        }
+    }
+
+
+    void FireLaser()
+    {      
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, -1.05f, 0), Quaternion.identity);
+     
+        _laserAudio.Play();       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -68,6 +93,8 @@ public class Enemy : MonoBehaviour
             //destroy Us 
             
             _audioSource.Play();
+            Destroy(GetComponent<BoxCollider2D>());
+
             Destroy(this.gameObject,2.8f);
         }
 
@@ -86,6 +113,7 @@ public class Enemy : MonoBehaviour
             //destroy us
             
             _audioSource.Play();
+            Destroy(GetComponent<BoxCollider2D>());
             Destroy(this.gameObject,2.8f);
             if (_player != null)
             {
